988c5d23539ed42f294979a649b6946c26013c9c
Shiraishi-Shodai <siranosuke1227@gmail.com>
2024-09-27 14:58:46 +0900

    2024/09/27 14:58

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,111 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { database } from "../../lib/firebaseConfig";
+import { ref, push, set, update } from "firebase/database";
 
 export default function HomeScreen() {
+  const writeUsers = () => {
+    const usersRef = ref(database, "comment");
+
+    // 削除
+    // set(usersRef, null)
+
+    set(usersRef, {
+      alanisawesome: {
+        date_of_birth: "June 23, 1912",
+        full_name: "Alan Turing",
+      },
+      gracehop: {
+        date_of_birth: "December 9, 1906",
+        full_name: "Grace Hopper",
+      },
+    })
+      .then(() => {
+        console.log("登録しました");
+      })
+      .catch((err) => console.log("エラーです"));
+  };
+
+  const updateUsers = () => {
+    const hopperRef = ref(database, "users");
+    update(hopperRef, {
+      "alanisawesome/nickname": "Alan The Machine",
+      "gracehop/nickname": "Amazing Grace",
+
+      // alanisawesome のような単一のキーパスが指定された場合、最初の子レベルにあるデータのみが更新され、
+      // 最初の子レベルを超えて渡されたあらゆるデータは set オペレーションとして扱われます。マルチパス動作により、
+      // データを上書きすることなく、より長いパス（alanisawesome/nickname など）を使用できます。このため、最初の例は 2 番目の例と異なります。
+      // alanisawesome: {
+      //   nickname: "Alan The Machine",
+      // },
+      // gracehop: {
+      //   nickname: "Amazing Grace",
+      // },
+    });
+  };
+
+  const pushPosts = () => {
+    const postRef = ref(database, "posts");
+    // 一意のキーはタイムスタンプに基づいているため、リストアイテムは自動的に時系列に並べ替えられます。
+    const newPostRef = push(postRef);
+    // set(newPostRef, {
+    //   author: "alanisawesome",
+    //   title: "The Turing Machine",
+    // });
+
+    // pushで生成される一意キーの取得
+    set(postRef, {
+      id: newPostRef.key,
+      author: "alanisawesome",
+      title: "The Turing Machine",
+    });
+  };
+
+  // transaction
+  // 特定のデータを atomically（原子的に）更新するために使用
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <Text style={styles.text}>Check For Firebase Integration!</Text>
+
+      <TouchableOpacity style={styles.button_container} onPress={writeUsers}>
+        <Text style={styles.button_text}>ユーザーを追加</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button_container} onPress={updateUsers}>
+        <Text style={styles.button_text}>ユーザーをアップデート</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button_container} onPress={pushPosts}>
+        <Text style={styles.button_text}>ポストを一意に追加</Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginTop: 48,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  text: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 24,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  button_text: {
+    textAlign: "center",
+    fontSize: 24,
+    color: "#1976d2",
+  },
+  button_container: {
+    borderRadius: 15,
+    flexDirection: "row",
+    margin: 16,
+    padding: 24,
+    justifyContent: "center",
+    backgroundColor: "#e6e6e6",
   },
 });
